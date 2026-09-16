c8b1d95b9e68988c1d47c07c7f44f08e51ff58fe
NucklearSnake <eldaro111@yandex.ru>
2026-09-16 20:39:01 +0700

    Integrate and fix sorting module

--- src/sorting/SelectionSortStrategy.java
+++ src/sorting/SelectionSortStrategy.java
@@ -1,24 +1,39 @@
 package sorting;
 
 import java.util.Comparator;
+import java.util.List;
 
 public class SelectionSortStrategy<T> implements SortStrategy<T> {
 
     @Override
-    public void sort(List<T> items, Comparator<T> comparator) {
+    public void sort(
+            List<T> items,
+            Comparator<T> comparator
+    ) {
         for (int i = 0; i < items.size() - 1; i++) {
             int minIndex = i;
 
             for (int j = i + 1; j < items.size(); j++) {
-                if (comparator.compare(items.get(j), items.get(minIndex)) < 0) {
+                if (comparator.compare(
+                        items.get(j),
+                        items.get(minIndex)
+                ) < 0) {
                     minIndex = j;
                 }
             }
 
             if (minIndex != i) {
                 T temp = items.get(i);
-                items.set(i, items.get(minIndex));
-                items.set(minIndex, temp);
+
+                items.set(
+                        i,
+                        items.get(minIndex)
+                );
+
+                items.set(
+                        minIndex,
+                        temp
+                );
             }
         }
     }
