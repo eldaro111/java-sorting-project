51d0bb302bb17aaa08d6209340a087bef2d28d04
virtualX <39302007+virtual-X-WAVE@users.noreply.github.com>
2026-09-16 13:51:00 +0300

    Revert "Sorting minor changes"
    This reverts commit ee1c3dbb594c2fc2e690697526b3ba70978bbce7.

--- src/sorting/BubbleSortStrategy.java
+++ src/sorting/BubbleSortStrategy.java
@@ -6,12 +6,12 @@ public class BubbleSortStrategy<T> implements SortStrategy<T> {
 
     @Override
     public void sort(List<T> items, Comparator<T> comparator) {
-        for (int i = 0; i < items.size() - 1; i++) {
-            for (int j = 0; j < items.size() - i - 1; j++) {
-                if (comparator.compare(items.get(j), items.get(j + 1)) > 0) {
-                    T temp = items.get(j);
-                    items.set(j, items.get(j + 1));
-                    items.set(j + 1, temp);
+        for (int i = 0; i < items.length - 1; i++) {
+            for (int j = 0; j < items.length - i - 1; j++) {
+                if (comparator.compare(items[j], items[j + 1]) > 0) {
+                    T temp = items[j];
+                    items[j] = items[j + 1];
+                    items[j + 1] = temp;
                 }
             }
         }
